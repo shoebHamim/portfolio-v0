633d2a3f733833f5393c6c90e4d307ceace5e976
Hamim <shoeb.hamim@gmail.com>
2025-07-24 10:42:31 +0600

    features section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
 import NavBar from './components/NavBar'
 import LogoSection from './sections/LogoSection'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
   return (
-    <>
+    <div className='mb-10'>
     <NavBar/>
     <Hero/>
     <ShowcaseSection/>
     <LogoSection/>
+    <FeatureCards/>
     
-    
-    </>
+    </div>
     
   )
 }
